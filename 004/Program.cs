ce806d448c12ba04a46ad869b8daf8a92107657a
9867387 <tservice48@yandex.ru>
2021-11-28 22:05:20 +0300

    найти самое большое из трех чисел

---
+++ 004/Program.cs
@@ -0,0 +1,12 @@
+﻿//Найти максимальное из трех чисел
+Console.Write(" Введите первое чесло: ");
+double a = Convert.ToDouble(Console.ReadLine());
+Console.Write(" Введите второе чесло: ");
+double b = Convert.ToDouble(Console.ReadLine());
+Console.Write(" Введите третье чесло: ");
+double c = Convert.ToDouble(Console.ReadLine());
+double max = 0;
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+Console.WriteLine(max + " это самое большое число ");
