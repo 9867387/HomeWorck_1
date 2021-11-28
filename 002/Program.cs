22ba5a9d72b6f8a6755d2e3f6e418f9303b13e1b
9867387 <tservice48@yandex.ru>
2021-11-28 21:30:18 +0300

    сравнение двух чесел

---
+++ 002/Program.cs
@@ -0,0 +1,19 @@
+﻿
+Console.Write("Введите первое число: ");
+double a = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите второе число: ");
+double b = Convert.ToDouble(Console.ReadLine());
+if (a > b)
+{
+    Console.WriteLine(a + " больше " + b);
+}
+
+if (a < b)
+{
+    Console.WriteLine(a + " меньше " + b);
+}
+
+if (a == b)
+{
+    Console.WriteLine(a + " равно " + b);
+}
