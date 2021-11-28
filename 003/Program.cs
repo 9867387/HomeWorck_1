3b1a684b45d01fcd4e9af833b6d88277fb5f8c4a
9867387 <tservice48@yandex.ru>
2021-11-28 21:46:38 +0300

    дни недели

---
+++ 003/Program.cs
@@ -0,0 +1,35 @@
+﻿//По заданному номеру дня недели вывести его название
+Console.Write("Введите день недели ");
+double a = Convert.ToDouble(Console.ReadLine());
+if(a==1)
+{
+    Console.WriteLine( "понедельник");
+}
+if(a==2)
+{
+    Console.WriteLine( "вторник");
+}
+if(a==3)
+{
+    Console.WriteLine("Среда");
+}
+if(a==4)
+{
+    Console.WriteLine("Четвер");
+}
+if(a==5)
+{
+    Console.WriteLine("Ух это же Пятница");
+}
+if(a==6)
+{
+    Console.WriteLine("Субота после пятницы блевота!");
+}
+if(a==7)
+{
+    Console.WriteLine("Воскресение");
+}
+if(a>7)
+{
+    Console.WriteLine("Ты тупой? Я сказал день недели!");
+}
