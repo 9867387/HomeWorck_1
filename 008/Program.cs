dbe38d542e14ffe455458a8945a896ae8a2da6bd
9867387 <tservice48@yandex.ru>
2021-12-01 00:21:13 +0300

    008

---
+++ 008/Program.cs
@@ -0,0 +1,9 @@
+﻿// Показать четные числа от 1 до N
+Console.WriteLine("Введите число");
+int a = int.Parse(Console.ReadLine());
+int count = 2;
+while(count<=a)
+{
+    Console.WriteLine(count);
+    count = count+2;
+}
