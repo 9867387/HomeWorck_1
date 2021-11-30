c9924218165eda615c9793a8be45f700c5019f3e
9867387 <tservice48@yandex.ru>
2021-12-01 00:08:00 +0300

    007

---
+++ 007/Program.cs
@@ -0,0 +1,9 @@
+﻿// Показать числа от -N до N
+Console.WriteLine("Введите число ");
+int a = int.Parse(Console.ReadLine());
+int count = -a;
+while (count <= a)
+{
+Console.WriteLine(count);
+count++;
+}
